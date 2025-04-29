971ebb4822f9780cd47d490ec48a72461251135b
roman <roman.r.daniel@gmail.com>
2025-04-29 12:22:34 +1000

    even cleaner empty mod :D

--- src/main/java/net/river/peacefulhostiles/PeacefulHostilesClient.java
+++
@@ -1,9 +0,0 @@
-package net.river.peacefulhostiles;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class PeacefulHostilesClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
